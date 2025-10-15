1c783f4adaa0960361e97b5e8ba9babffc624fc8
Frank-Muzi <frankmuzim@gmail.com>
2025-10-15 18:37:24 +0200

    url

--- src/Pages/HomePredictorForm.jsx
+++ src/Pages/HomePredictorForm.jsx
@@ -50,7 +50,7 @@ export default function HomePredictorForm() {
         Garage: form.garage,
       };
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://house-price-predictor-ai-project-4.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
