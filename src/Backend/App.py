e192aa7be54a8a40d3115069512950e70bd088ae
Frank-Muzi <frankmuzim@gmail.com>
2025-10-15 18:22:14 +0200

    render

--- src/Backend/App.py
+++ src/Backend/App.py
@@ -65,5 +65,10 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 
+@app.route('/')
+def home():
+    return jsonify({"message": "Backend is running!"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
